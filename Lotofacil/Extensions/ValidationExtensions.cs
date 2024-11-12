c85cfc62b1fcce7c012639a7db27c10c991055fd
Nathan Farias <nathan90055@gmail.com>
2024-11-11 22:28:11 -0300

    FEAT: Implementar a classe ValidationExtensions para auxiliar no uso do FluentValidations

---
+++ Lotofacil/Extensions/ValidationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Lotofacil.Extensions
+{
+    public static class ValidationExtensions
+    {
+        public static void AddToModelState(this ValidationResult result, ModelStateDictionary modelState)
+        {
+            foreach (var error in result.Errors)
+            {
+                modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+
+        }
+    }
+}
+/*Esse método percorre a lista de Errors no ValidationResult e adiciona cada erro ao ModelState, 
+ * com a propriedade PropertyName como chave e a ErrorMessage como valor.
+ O ASP.NET usa o ModelState para armazenar mensagens de erro de validação e exibi-las na view*/
